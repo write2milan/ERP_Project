092b8bd02b9371b40571adf1a2c5061d9e4cc6aa
Krishnendu Chakraborty <krishnendu.chakraborty81@gmail.com>
2017-03-21 00:10:09 +0530

    Product master form  - strarted

---
+++ ERP_WebDesign_Main/Controllers/MasterDataController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_WebDesign_Main.Controllers
+{
+    public class MasterDataController : BaseController
+    {
+        // GET: MasterData
+        [AuthenticationFilter]
+        public ActionResult ProductMaster_Index()
+        {
+            Model_BL.MasterData_BL.ProductMaster_BL objProductMasterBL = new Model_BL.MasterData_BL.ProductMaster_BL();
+            return View(objProductMasterBL.GetAllItems());
+        }
+
+        // GET: MasterData/Details/5
+        [AuthenticationFilter]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MasterData/Create
+        [AuthenticationFilter]
+        public ActionResult ProductMaster_Create()
+        {
+            return View();
+        }
+
+        // POST: MasterData/Create
+        [AuthenticationFilter]
+        [HttpPost]
+        public ActionResult ProductMaster_Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Model_BL.MasterData_BL.ProductMaster_BL objProductMasterBL = new Model_BL.MasterData_BL.ProductMaster_BL();
+                objProductMasterBL.InsertData(collection);
+                return RedirectToAction("ProductMaster_Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MasterData/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: MasterData/Edit/5
+        [AuthenticationFilter]
+        [HttpPost]
+        public ActionResult ProductMaster_Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MasterData/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        // POST: MasterData/Delete/5
+        [AuthenticationFilter]
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ERP_WebDesign_Main/Model_BL/MasterData_BL/ProductMaster_BL.cs
@@ -0,0 +1,60 @@
+﻿using ERP_WebDesign_Main.Model_Entity_DB;
+using ERP_WebDesign_Main.Models.MasterData_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERP_WebDesign_Main.Model_BL.MasterData_BL
+{
+    public class ProductMaster_BL
+    {
+        public IEnumerable<ProductMaster_Model> GetAllItems()
+        {
+            IEnumerable<ProductMaster_Model> objProdMasterCollection = null;
+            try
+            {
+                using (ERP_DEMOEntities objContext = new ERP_DEMOEntities())
+                {
+                    objProdMasterCollection = (from each in objContext.tbl_ProductMaster
+                                               select new ProductMaster_Model
+                                               {
+                                                   ProductID = each.ProductID,
+                                                   ProductDesc = each.ProductDesc
+                                               }).ToList<ProductMaster_Model>();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return objProdMasterCollection;
+
+        }
+        public void InsertData(System.Web.Mvc.FormCollection collection)
+        {
+            try
+            {
+                using (ERP_DEMOEntities objContext = new ERP_DEMOEntities())
+                {
+                    objContext.tbl_ProductMaster.Add(new tbl_ProductMaster
+                    {
+                        ProductID = Convert.ToString(collection["ProductID"]),
+                        ProductDesc = Convert.ToString(collection["ProductDesc"])
+                    });
+                    objContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+        }
+    }
+}
