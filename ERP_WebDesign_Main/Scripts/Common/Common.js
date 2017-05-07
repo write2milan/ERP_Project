36644852b4c37a086861d59088547674d0d457f2
Krishnendu Chakraborty <krishnendu.chakraborty81@gmail.com>
2017-05-07 15:56:48 +0530

    Add tooltip for codification material code

--- ERP_WebDesign_Main/Scripts/Common/Common.js
+++ ERP_WebDesign_Main/Scripts/Common/Common.js
@@ -2,6 +2,9 @@
     var ERP_APP = angular.module('ERP-APP', ['ERP-CONTROLLER', 'ERP-SERVICE']);
     var controllerModule = angular.module('ERP-CONTROLLER', []);
     var serviceModule = angular.module('ERP-SERVICE', []);
+    angular.element(document).ready(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
 })();
 
