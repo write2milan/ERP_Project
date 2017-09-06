a80ab717c092e9f1064e1846431b5c249f36012e
smilee081 <krishnendu.chakraborty81@gmail.com>
2017-09-06 23:14:03 +0530

    Change model

--- ERP_WebDesign_Main/Models/MasterData_Model/Codification_Model.cs
+++ ERP_WebDesign_Main/Models/MasterData_Model/Codification_Model.cs
@@ -1,4 +1,4 @@
-﻿using ERP_WebDesign_Main.Resources;
+using ERP_WebDesign_Main.Resources;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -26,12 +26,45 @@ namespace ERP_WebDesign_Main.Models.MasterData_Model
         public string SpecificationID { get; set; }
         [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "CODI_RACK")]
         public string RackID { get; set; }
+
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "ITEM_DISPLAYNAME")]
+        public string ItemDisplayName { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "ITEM_CODE")]
+        public string ItemCode { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "GROUP_DISPLAYNAME")]
+        public string GroupDisplayName { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "GROUP_CODE")]
+        public string GroupCode { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "SPEC_DISPLAYNAME")]
+        public string SpecificationDisplayName { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "SPEC_CODE")]
+        public string SpecificationCode { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "RACK_DISPLAYNAME")]
+        public string RackDisplayName { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "RACK_CODE")]
+        public string RackCode { get; set; }
         public List<CodificationSelectListItem> Items { get; set; }
         public List<CodificationSelectListItem> Groups { get; set; }
         public List<CodificationSelectListItem> Specifications { get; set; }
         public List<CodificationSelectListItem> Racks { get; set; }
         public string ValidationMessageForDropdowns { get; set; }
 
+    }
+
+    public class CodificationFilterItems
+    {
+        public List<CodificationSelectListItem> Items { get; set; }
+        public List<CodificationSelectListItem> Groups { get; set; }
+        public List<CodificationSelectListItem> Specifications { get; set; }
+        public List<CodificationSelectListItem> Racks { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "CODI_ITEM")]
+        public string ItemID { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "CODI_GROUP")]
+        public string GroupID { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "CODI_SPEC")]
+        public string SpecificationID { get; set; }
+        [Display(ResourceType = typeof(ERP_WebDesign_MasterModelResource), Name = "CODI_RACK")]
+        public string RackID { get; set; }
     }
 
     public class CodificationSelectListItem
@@ -52,5 +85,12 @@ namespace ERP_WebDesign_Main.Models.MasterData_Model
         public int PageNo { get; set; }
         public int PageSize { get; set; }
         public string NoRecordFound { get; set; }
+
+    }
+
+    public class CodificationWrapper
+    {
+        public Codification Codi { get; set; }
+        public CodificationFilterItems CodiFilterItems { get; set; }
     }
 }
